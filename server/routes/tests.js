1f173bfe9cd348331f5bf4d404b9cda3d174bda2
corwin-alex <119405724+corwin-alex@users.noreply.github.com>
2026-04-13 14:11:32 +0300

    Update tests.js

--- server/routes/tests.js
+++ server/routes/tests.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const pool = require('../db');
+const { pool } = require('../db');
 require('dotenv').config();
 
 const router = express.Router();
